dd28911f4287409a20b329c0622ec4f5413c28c2
kaphie <bkafrika144@gmail.com>
2020-05-23 18:05:41 +0300

    adding a srech function to views.py

--- src/pics/urls.py
+++ src/pics/urls.py
@@ -1,6 +1,11 @@
-from django.conf.urls import url
+from django.urls import path
 from . import views
+from django.conf.urls import url
 
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
+    path('', views.pictures_index, name='pictures_index'),
+    url(r'^search/', views.search_results, name='search_results'),
+    path('<int:pk>/', views.pictures_detail, name='pictures_detail'),
+    path('<category>/', views.pictures_category, name="pictures_category"),
+    path('<location>/', views.pictures_location, name="pictures_location"),
 ]
--- src/pics/views.py
+++ src/pics/views.py
@@ -1,6 +1,55 @@
 from django.shortcuts import render
-from django.http  import HttpResponse
+from pics.models import Pictures, Category, Location
 
 # Create your views here.
-def pictures(request):
-    return render(request, 'pictures.html')
+def pictures_index(request):
+    pictures = Pictures.objects.all().order_by('-last_modified')
+    location = Location.objects.all()
+    context = {
+        'pictures':pictures,
+        'location':location
+    }
+    return render(request, 'pictures.html', context)
+
+def pictures_detail(request, pk):
+    picture = Pictures.objects.get(pk=pk)
+    location = Location.objects.all()
+    context = {
+        'picture':picture,
+        'location':location
+    }
+    return render(request, 'pictures_detail.html', context)
+
+def pictures_category(request, category):
+    pictures = Pictures.objects.filter(image_category__category__contains=category).order_by('-created_on')
+    location = Location.objects.all()
+    context = {
+        "category": category,
+        "pictures": pictures,
+        'location':location
+    }
+    return render(request, "pictures_category.html", context)
+
+def pictures_location(request, location):
+    pictures = Pictures.objects.filter(image_location__location__contains=location).order_by('-created_on')
+    location = Location.objects.all()
+    context = {
+        "location": location,
+        "pictures": pictures,
+        'location':location
+    }
+    return render(request, "pictures_location.html", context)
+
+def search_results(request):
+    location = Location.objects.all()
+
+    if 'search_item' in request.GET and request.GET["search_item"]:
+        search_term = request.GET.get("search_item")
+        searched_pictures = Pictures.search_by_category(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message,"searched_pictures": searched_pictures,'location':location})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message, 'location':location})
