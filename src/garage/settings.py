a0a6c9b404a61e2de24e34cd8e8f05f9ed3e1ad3
kaphie <bkafrika144@gmail.com>
2020-05-23 19:49:58 +0300

    updating settings

--- src/garage/settings.py
+++ src/garage/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pics',
+    'pics.apps.PicsConfig',
     'bootstrap3',
 ]
 
