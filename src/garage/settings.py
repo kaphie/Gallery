5b4d0af2281546a20f434a8e1cea763620293f9c
kaphie <bkafrika144@gmail.com>
2020-05-23 19:32:59 +0300

    updating settings.py

--- src/garage/settings.py
+++ src/garage/settings.py
@@ -128,3 +128,8 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
