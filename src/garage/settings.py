f6a379ddf5bc4e7e7ee89b21197da57d034ea3db
kaphie <bkafrika144@gmail.com>
2020-05-23 18:29:25 +0300

    adding content to models.py

--- src/garage/settings.py
+++ src/garage/settings.py
@@ -77,10 +77,12 @@ WSGI_APPLICATION = 'garage.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'garage',
         'USER': 'user',
-    'PASSWORD':'tyrrelhaslay718',
+        'PASSWORD': 'tyrrelhaslay718',
+
+        
     }
 }
 
@@ -122,6 +124,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
