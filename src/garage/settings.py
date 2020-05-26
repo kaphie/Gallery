381a0fe8a3e03502efeff3af355b5e5fd0680215
kaphie <bkafrika144@gmail.com>
2020-05-26 19:37:02 +0300

    final push

--- src/garage/settings.py
+++ src/garage/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'hx)x_gnw(_31vqb!ar=@$btwwrcuq5%p9!_tn(f*4(bwi6*7_9'
+SECRET_KEY =os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG")
 
 ALLOWED_HOSTS = []
 
